4737eaf947c0079e0e1a9c5107ed1caa5bd11e1d
mukundan1 <mukundan155@gmail.com>
2025-06-13 18:07:54 +0530

    step 5 is store vectordb

---
+++ storevector.py
@@ -0,0 +1,24 @@
+def insert_publications(collection: chromadb.Collection, publications: list[str]):
+    """
+    Insert documents into a ChromaDB collection.
+
+    Args:
+        collection (chromadb.Collection): The collection to insert documents into
+        publications (list[str]): The documents to insert
+
+    Returns:
+        None
+    """
+    next_id = collection.count()
+
+    for publication in publications:
+        chunked_publication = chunk_publication(publication)
+        embeddings = embed_documents(chunked_publication)
+        ids = list(range(next_id, next_id + len(chunked_publication)))
+        ids = [f"document_{id}" for id in ids]
+        collection.add(
+            embeddings=embeddings,
+            ids=ids,
+            documents=chunked_publication,
+        )
+        next_id += len(chunked_publication)
