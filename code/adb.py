7baf4bb1691a0b60c82cae3599aa8d41e2515226
mukundan1 <mukundan155@gmail.com>
2025-06-13 21:52:00 +0530

    summa

--- code/adb.py
+++ code/adb.py
@@ -37,4 +37,31 @@ def chunk_research_paper(paper_content, title):
     
     return chunk_data
 
-    
+
+#Insertion into vector database   
+def insert_publications(collection: chromadb.Collection, publications: list[str]):
+    """
+    Insert documents into a ChromaDB collection.
+
+    Args:
+        collection (chromadb.Collection): The collection to insert documents into
+        publications (list[str]): The documents to insert
+
+    Returns:
+        None
+    """
+    next_id = collection.count()
+
+    for publication in publications:
+        chunked_publication = chunk_publication(publication)
+        embeddings = embed_documents(chunked_publication)
+        ids = list(range(next_id, next_id + len(chunked_publication)))
+        ids = [f"document_{id}" for id in ids]
+        collection.add(
+            embeddings=embeddings,
+            ids=ids,
+            documents=chunked_publication,
+        )
+        next_id += len(chunked_publication)
+
+
